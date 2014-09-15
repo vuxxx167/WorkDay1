3b1ced8f799c1c52e196e7ceb64f624dca99ca16
idsc3101 <idsc3101@gmail.com>
2014-09-15 12:29:58 -0500

    Created the project skeletons

---
+++ FirstName/FirstName/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFullName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnFirstName = New System.Windows.Forms.Button()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFullName
+        '
+        Me.txtFullName.Location = New System.Drawing.Point(154, 35)
+        Me.txtFullName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtFullName.Name = "txtFullName"
+        Me.txtFullName.Size = New System.Drawing.Size(153, 20)
+        Me.txtFullName.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 37)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(141, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Please Type Your Full Name"
+        '
+        'btnFirstName
+        '
+        Me.btnFirstName.Location = New System.Drawing.Point(109, 67)
+        Me.btnFirstName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnFirstName.Name = "btnFirstName"
+        Me.btnFirstName.Size = New System.Drawing.Size(113, 21)
+        Me.btnFirstName.TabIndex = 2
+        Me.btnFirstName.Text = "Extract First Name"
+        Me.btnFirstName.UseVisualStyleBackColor = True
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(154, 114)
+        Me.txtFirstName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.ReadOnly = True
+        Me.txtFirstName.Size = New System.Drawing.Size(153, 20)
+        Me.txtFirstName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(57, 116)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Your First Name is"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(341, 177)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.btnFirstName)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFullName)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "FirstName"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtFullName As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnFirstName As System.Windows.Forms.Button
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ FirstName/FirstName/Form1.vb
@@ -0,0 +1,11 @@
+﻿Option Strict On
+
+Public Class Form1
+
+    Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
+        ' Please write your code here
+
+        txtFirstName.Text = "Not implemented yet"
+
+    End Sub
+End Class
